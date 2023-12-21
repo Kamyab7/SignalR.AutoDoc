6f7741706030d3fb58d99bb30771d673c5778b5b
Kamyab7 <valipourkamyab.official@gmail.com>
2023-12-21 13:46:55 +0330

    middleware added

---
+++ src/SignalR.AutoDoc/AutoDocUIBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace SignalR.AutoDoc
+{
+    public static class AutoDocUIBuilderExtensions
+    {
+        public static IApplicationBuilder UseAutoDocUI(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<AutoDocUIMiddleware>();
+        }
+    }
+}
---
+++ src/SignalR.AutoDoc/AutoDocUIMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+
+namespace SignalR.AutoDoc
+{
+    internal class AutoDocUIMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AutoDocUIMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var httpMethod = httpContext.Request.Method;
+            var path = httpContext.Request.Path.Value;
+
+            // If the RoutePrefix is requested (with or without trailing slash), redirect to index URL
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"^/?{Regex.Escape("autodoc")}/?$", RegexOptions.IgnoreCase))
+            {
+                // Use relative redirect to support proxy environments
+                var relativeIndexUrl = string.IsNullOrEmpty(path) || path.EndsWith("/")
+                    ? "index.html"
+                    : $"{path.Split('/').Last()}/index.html";
+
+                RespondWithRedirect(httpContext.Response, relativeIndexUrl);
+                return;
+            }
+
+            await _next(httpContext);
+        }
+
+        private void RespondWithRedirect(HttpResponse response, string location)
+        {
+            response.StatusCode = 301;
+            response.Headers["Location"] = location;
+        }
+    }
+}
