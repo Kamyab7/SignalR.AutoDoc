096d351cf1b3ae898401642e866b7ed5a4e47e38
Kamyab7 <valipourkamyab.official@gmail.com>
2023-12-21 15:09:39 +0330

    SignalR client added

--- tests/samples/Basic/SampleHub.cs
+++ tests/samples/Basic/SampleHub.cs
@@ -8,4 +8,12 @@ public class SampleHub : Hub
     {
         await Clients.All.SendAsync("ReceiveMessage", message);
     }
+
+    public override async Task OnConnectedAsync()
+    {
+        // This newMessage call is what is not being received on the front end
+        await Clients.Caller.SendAsync("OnConnected", "Connected!");
+
+        await base.OnConnectedAsync();
+    }
 }
